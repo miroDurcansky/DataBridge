4acb0fb24033ef117fc13acb1c4deed58bcdff24
miroslavdurcansky <miro.durcansky@gmail.com>
2023-02-28 14:06:17 +0100

    test flyway script

---
+++ src/main/resources/db/migration/V1__create_test_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE title_basics
+(
+    tconst          text PRIMARY KEY,
+    title_type      text,
+    primary_title   text
+);
+
+CREATE TABLE name_basics
+(
+    nconst             text PRIMARY KEY,
+    primary_name       text
+);
