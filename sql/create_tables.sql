0fa20b0050947b0e2db5502641ac1ccb1abc1d45
miroslavdurcansky <miro.durcansky@gmail.com>
2023-03-02 14:02:02 +0100

    change name of columns in the table tag_descs

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS tag_descs_ab_as_ke (
-    id SERIAL NOT NULL PRIMARY KEY,
-    name varchar(64) NOT NULL DEFAULT 'noname',
-    description varchar(256) NOT NULL DEFAULT 'NONE',
-    type varchar(32) NOT NULL DEFAULT 'PHYSICAL',
-    physicalType varchar(32) NOT NULL DEFAULT 'NUMERIC',
-    physicalDecimals int NOT NULL DEFAULT 0,
-    physicalUnit varchar(16) NOT NULL DEFAULT 'units',
-    physicalMin double precision NOT NULL DEFAULT 0,
-    physicalMax double precision NOT NULL DEFAULT 1,
-    physicalMinAlarm double precision NOT NULL DEFAULT 0,
-    physicalMaxAlarm double precision NOT NULL DEFAULT 1,
-    physicalMinWarn double precision NOT NULL DEFAULT 0,
-    physicalMaxWarn double precision NOT NULL DEFAULT 1
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "name" varchar(64) NOT NULL DEFAULT 'noname',
+    "description" varchar(256) NOT NULL DEFAULT 'NONE',
+    "type" varchar(32) NOT NULL DEFAULT 'PHYSICAL',
+    "physical_type" varchar(32) NOT NULL DEFAULT 'NUMERIC',
+    "physical_decimals" int NOT NULL DEFAULT 0,
+    "physical_unit" varchar(16) NOT NULL DEFAULT 'units',
+    "physical_min" double precision NOT NULL DEFAULT 0,
+    "physical_max" double precision NOT NULL DEFAULT 1,
+    "physical_min_alarm" double precision NOT NULL DEFAULT 0,
+    "physical_max_alarm" double precision NOT NULL DEFAULT 1,
+    "physical_min_warn" double precision NOT NULL DEFAULT 0,
+    "physical_max_warn" double precision NOT NULL DEFAULT 1
 );
 
 CREATE TABLE IF NOT EXISTS tag_vals_ab_as_ke (
