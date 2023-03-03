5660cd3cbe7a26be8d03d55ef7d2d46224dc3f86
miroslavdurcansky <miro.durcansky@gmail.com>
2023-03-03 22:53:09 +0100

    query Unipi findBetweenDates

--- src/main/java/sk/energodata/DataBridge/Model/UnipiValue.java
+++ src/main/java/sk/energodata/DataBridge/Model/UnipiValue.java
@@ -5,8 +5,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-@Data
 @Table(name = "tag_vals_ab_as_ke")
 public class UnipiValue {
 
@@ -15,5 +15,58 @@ public class UnipiValue {
     private LocalDateTime valueTime;
     private double value;
     private Boolean valid;
+    private Long descsId;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getValueTime() {
+        return valueTime;
+    }
+
+    public void setValueTime(LocalDateTime valueTime) {
+        this.valueTime = valueTime;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public Boolean getValid() {
+        return valid;
+    }
+
+    public void setValid(Boolean valid) {
+        this.valid = valid;
+    }
+
+    public Long getDescsId() {
+        return descsId;
+    }
+
+    public void setDescsId(Long descsId) {
+        this.descsId = descsId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UnipiValue that = (UnipiValue) o;
+        return Double.compare(that.value, value) == 0 &&
+                Objects.equals(valueTime, that.valueTime);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(valueTime, value);
+    }
 }
--- src/main/java/sk/energodata/DataBridge/UnipiRepository.java
+++ src/main/java/sk/energodata/DataBridge/UnipiRepository.java
@@ -2,9 +2,12 @@ package sk.energodata.DataBridge;
 
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sk.energodata.DataBridge.Model.Unipi;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,5 +15,12 @@ public interface UnipiRepository extends CrudRepository<Unipi, Long> {
 
     @Query("SELECT * FROM tag_descs_ab_as_ke WHERE name = :name")
     Optional<Unipi> findByName(String name);
+
+    @Query("SELECT DISTINCT u.*, uv.* \n" +
+            "FROM tag_descs_ab_as_ke u \n" +
+            "JOIN tag_vals_ab_as_ke uv ON u.id = uv.descs_id \n" +
+            "WHERE uv.value_time BETWEEN :startDate AND :endDate")
+    List<Unipi> findBetweenDates(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
+
 
 }
