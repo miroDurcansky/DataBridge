65eb240e2b77f5c6114b8588d4dd2b181d4b7dff
miroslavdurcansky <miro.durcansky@gmail.com>
2023-03-09 13:39:27 +0100

    test file

--- src/test/java/sk/energodata/DataBridge/DataBridgeApplicationTests.java
+++ src/test/java/sk/energodata/DataBridge/DataBridgeApplicationTests.java
@@ -3,14 +3,22 @@ package sk.energodata.DataBridge;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import sk.energodata.DataBridge.Models.Student;
+import sk.energodata.DataBridge.Models.Subject;
+import sk.energodata.DataBridge.Models.UnipiValue;
+import sk.energodata.DataBridge.Repository.StudentRepository;
+import sk.energodata.DataBridge.Repository.UnipiValueRepository;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootTest
 class DataBridgeApplicationTests {
-	@Test
-	void contextLoads() {
 
-//		unipiValueRepository.saveOrUpdateValue(LocalDateTime.of(2022, Month.AUGUST, 15, 10, 30), 2, 25.0, Boolean.FALSE);
-//		System.out.println("tu som");
+
+	void contextLoads() {
 
 
 	}
