e8327a7c0e0db2da38ab5519570bfe8c9761934c
miroslavdurcansky <miro.durcansky@gmail.com>
2023-03-09 18:48:16 +0100

    get first json object by rest api

--- src/main/java/sk/energodata/DataBridge/Controllers/HelloWorldController.java
+++ src/main/java/sk/energodata/DataBridge/Controllers/HelloWorldController.java
@@ -1,12 +1,24 @@
 package sk.energodata.DataBridge.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sk.energodata.DataBridge.Models.Unipi;
+import sk.energodata.DataBridge.Services.UnipiDao;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    UnipiDao unipiDao;
+
     @GetMapping("/hello")
-    public String HelloWorld() {
-        return "Hello World!!";
+    public List<Unipi> HelloWorld() {
+
+        return unipiDao.getUnipiWithValuesFromTo(LocalDateTime.now().minusMinutes(10), LocalDateTime.now());
     }
 }
